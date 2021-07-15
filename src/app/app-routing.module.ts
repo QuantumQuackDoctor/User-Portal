638238f4ce1e48c354d5488248e897883a1b634f
Ezra Mitchell <ezra.mitchell@smoothstack.com>
2021-07-14 21:20:48 -0600

    router and pages created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './pages/account/account.component'
+import { HomeComponent } from './pages/home/home.component'
+import { LoginComponent } from './pages/login/login.component'
+import { SearchComponent } from './pages/search/search.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'account', component: AccountComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
