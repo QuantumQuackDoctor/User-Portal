3a0c06cd7d14dc96d39d4868c5a193fbf1cfefbe
Ezra Mitchell <ezra.mitchell@smoothstack.com>
2021-08-02 10:19:34 -0600

    Added logout button, account page now redirects to home if not authenticated

--- src/app/pages/account/user-settings/user-settings.component.ts
+++ src/app/pages/account/user-settings/user-settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/User';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-user-settings',
@@ -8,8 +10,18 @@ import { User } from 'src/app/models/User';
 })
 export class UserSettingsComponent implements OnInit {
   @Input() user: User | undefined;
+  authService: AuthService;
+  router: Router;
 
-  constructor() {}
+  constructor(authService: AuthService, router: Router) {
+    this.authService = authService;
+    this.router = router;
+  }
 
   ngOnInit(): void {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/home']);
+  }
 }
