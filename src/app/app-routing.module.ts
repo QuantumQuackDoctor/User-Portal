dda8987d99f1b78ac832380e8caae947aef9a870
Ezra Mitchell <ezra.mitchell@smoothstack.com>
2021-08-11 15:49:30 -0600

    removing unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { OrdersComponent } from "./pages/orders/orders.component";
+import { OrdersComponent } from './pages/orders/orders.component';
 import { AccountComponent } from './pages/account/account.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -8,7 +8,6 @@ import { SearchComponent } from './pages/search/search.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { RestaurantComponent } from './pages/restaurant/restaurant.component';
 import { ShopComponent } from './pages/shop/shop.component';
-import { PendingOrdersComponent } from './pending-orders/pending-orders.component';
 import { LoginFormComponent } from './pages/login/login-form/login-form.component';
 import { RegisterFormComponent } from './pages/login/register-form/register-form.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
