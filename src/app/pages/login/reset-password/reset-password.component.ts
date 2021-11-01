5b10dccccee04106edd8bb3db4b9755c00559f6b
Ezra Mitchell <ezra.mitchell@smoothstack.com>
2021-11-01 12:38:50 -0600

    password reset finished

--- src/app/pages/login/reset-password/reset-password.component.ts
+++ src/app/pages/login/reset-password/reset-password.component.ts
@@ -1,20 +1,24 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {
-  AbstractControl,
   FormControl,
   FormGroup,
   ValidatorFn,
   Validators,
 } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrls: ['../shared.css'],
 })
-export class ResetPasswordComponent implements OnInit {
+export class ResetPasswordComponent {
   formGroup: FormGroup;
+  errorMessage = '';
+  hasSubmitted = false;
+
   passwordValidator: ValidatorFn = (fg: FormGroup) => {
     const password1 = fg.get('password').value;
     const password2 = fg.get('parityPassword').value;
@@ -23,7 +27,12 @@ export class ResetPasswordComponent implements OnInit {
       ? null
       : { differentPasswords: { value: password2 } };
   };
-  constructor(private route: ActivatedRoute) {
+
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.formGroup = new FormGroup(
       {
         password: new FormControl(null, {
@@ -37,7 +46,37 @@ export class ResetPasswordComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {
-    console.log(this.route.params);
+  onSubmit() {
+    this.formGroup.updateValueAndValidity();
+    if (this.formGroup.valid) {
+      this.errorMessage = '';
+      this.hasSubmitted = true;
+      let data = {
+        newPassword: this.formGroup.value.password,
+        token: this.route.snapshot.params.token,
+      };
+      this.authService.resetPassword(data).subscribe(
+        () => {
+          this.router.navigate(['/account/login']);
+        },
+        (err: HttpErrorResponse) => {
+          switch (err.status) {
+            case 410:
+              this.errorMessage = '*password reset expired';
+              break;
+            case 404:
+              this.errorMessage = '*password reset token does not exist';
+              break;
+            case 500:
+              this.errorMessage = '*server error';
+          }
+          this.hasSubmitted = false;
+        }
+      );
+    } else {
+      if (this.formGroup.errors.differentPasswords !== null) {
+        this.errorMessage = '*Passwords must match';
+      }
+    }
   }
 }
